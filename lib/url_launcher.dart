e163cc6e67301a2fdb62ef544a375c848cbb06c4
najeira <najeira@gmail.com>
2022-12-09 16:43:28 +0900

    feat: change LaunchMode to externalApplication

--- lib/url_launcher.dart
+++ lib/url_launcher.dart
@@ -3,6 +3,6 @@ import 'package:url_launcher/url_launcher_string.dart' as lib;
 Future<bool> launchUrl(String str) {
   return lib.launchUrlString(
     str,
-    mode: lib.LaunchMode.platformDefault,
+    mode: lib.LaunchMode.externalApplication,
   );
 }
